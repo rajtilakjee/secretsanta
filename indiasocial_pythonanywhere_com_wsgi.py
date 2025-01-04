e9fd0055cd0902a31a92231460c722406a10637b
Rajtilak Bhattacharjee <rajtilak.blog@gmail.com>
2025-01-04 20:00:49 +0530

    Add WSGI config file for PythonAnywhere

---
+++ indiasocial_pythonanywhere_com_wsgi.py
@@ -0,0 +1,20 @@
+# This file contains the WSGI configuration required to serve up your
+# web application at http://<your-username>.pythonanywhere.com/
+# It works by setting the variable 'application' to a WSGI handler of some
+# description.
+#
+# The below has been auto-generated for your Flask project
+
+import sys
+import os
+from dotenv import load_dotenv
+
+# add your project directory to the sys.path
+project_home = '/home/indiasocial/secretsanta'
+if project_home not in sys.path:
+    sys.path = [project_home] + sys.path
+
+load_dotenv(os.path.join(project_home, '.env'))
+
+# import flask app but need to call it "application" for WSGI to work
+from app import app as application  # noqa
